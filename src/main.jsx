37e7038574c2cf333614bc5c78e794b1295b7076
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-12 22:54:42 +0600

    Add react-router-dom for routing and update main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Routes from './Router/Routes'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={Routes} />
   </StrictMode>,
 )
