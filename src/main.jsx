c1a5fc30fb3428b461d61340e3d39a0f4e589ab6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-12 22:33:41 +0600

    Add Roboto font and update dependencies; remove old CSS styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Routes from "./Router/Routes.jsx";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
