22fa8af9b714beac0a2023b22436a04ce588deef
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-27 21:39:13 +0600

    Replace text logo with SVG logo in Navbar component

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import { IoClose, IoMenu } from "react-icons/io5";
 import { useState } from "react";
 import { routes } from "../../../Utils/menu";
+import logo from "../../../assets/logo/logo-white.svg";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 w-full py-[1.3rem] px-[2%] lg:py-5 lg:px-[2%] lg:flex lg:justify-between lg:items-center z-[100] before:content-[''] before:top-0 before:left-0 before:absolute before:w-full before:h-full before:bg-gradient-back before:backdrop-blur-[50px] before:z-[-1] shadow-xl shadow-gradient-back ">
       <a href="/" className="text-4xl text-white font-bold">
-        Logo
+       <img src={logo} alt="" />
       </a>
       <label
         htmlFor=""
