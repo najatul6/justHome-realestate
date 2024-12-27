033f42325585946d9dc34771a7da8c2ecf990d99
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-27 22:42:57 +0600

    Refactor Navbar component for improved readability and structure

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <header className="fixed top-0 left-0 w-full py-[1.3rem] px-[2%] lg:py-5 lg:px-[2%] lg:flex lg:justify-between lg:items-center z-[100] before:content-[''] before:top-0 before:left-0 before:absolute before:w-full before:h-full before:bg-black lg:before:bg-transparent before:z-[-1] shadow-xl shadow-gradient-back ">
       <a href="/" className="text-4xl text-white font-bold">
-       <img src={logo} alt="Brand logo" />
+        <img src={logo} alt="Brand logo" />
       </a>
       <label
         htmlFor=""
@@ -28,7 +28,9 @@ const NavBar = () => {
             <li
               key={route?.id}
               className={`${
-                isOpen ? "translate-y-0 transition-all duration-300" : "translate-y-[-50px] transition-all duration-700"
+                isOpen
+                  ? "translate-y-0 transition-all duration-300"
+                  : "translate-y-[-50px] transition-all duration-700"
               } text-white font-medium text-lg lg:text-xl my-[1.5rem] lg:my-0 mx-0 lg:ml-10 block text-center duration-500 transition lg:translate-y-0 `}
             >
               <a href={route?.path} className=" px-2 py-3 block">
@@ -38,7 +40,10 @@ const NavBar = () => {
           ))}
         </ul>
       </nav>
-      <a href="/" className="hover:bg-white hover:text-yellow-700 border-2 rounded-xl text-lg lg:text-xl capitalize font-bold text-white px-5 py-2 ml-5 md:ml-[200px] lg:ml-0">
+      <a
+        href="/"
+        className="hover:bg-white hover:text-yellow-700 border-2 rounded-xl text-lg lg:text-xl capitalize font-bold text-white px-5 py-2 ml-5 md:ml-[200px] lg:ml-0"
+      >
         Open app
       </a>
     </header>
