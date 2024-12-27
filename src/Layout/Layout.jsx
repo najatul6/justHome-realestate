04a86fe076174868ee0ddea4505cb9fb477eb8c4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-27 21:31:45 +0600

    Add NavBar component to Layout for improved navigation

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
+import NavBar from "../Components/Shared/Navbar/Navbar"
 
 const Layout = () => {
   return (
     <div>
+      <NavBar/>
         <Outlet/>
     </div>
   )
